67fa0bb9234c043600de1346753ad3a3fbdeff1a
vlinux <oldmanpushcart@gmail.com>
2022-02-12 23:18:47 +0800

    修正安装脚本

--- bin/install-local.sh
+++ bin/install-local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-projects[i++]="io.github.athingx.athing.aliyun.tunnel:tunnel-thing"
-projects[i++]="io.github.athingx.athing.aliyun.tunnel:tunnel-thing-impl"
+projects[i++]="io.github.athingx.athing.thing.tunnel:thing-tunnel"
+projects[i++]="io.github.athingx.athing.thing.tunnel:thing-tunnel-aliyun"
 
 mvn clean install \
   -f ../pom.xml \
